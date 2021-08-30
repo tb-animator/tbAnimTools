7dfb4731a60264686e84872d27602d4ca6172c73
brimblashman@gmail.com <J33nLi4Lun>
2021-08-30 13:05:11 -0700

    adding support for finding compiled python files

--- pluginLookup.py
+++ pluginLookup.py
@@ -53,56 +53,57 @@ class ClassFinder(object):
         self.proToolsBaseDirectory = 'proApps'
         self.toolsDirectory = os.path.join(self.directory, self.toolsBaseDirectory)
         self.proToolsDirectory = os.path.join(self.directory, self.proToolsBaseDirectory)
-        #print 'startup', self.toolsDirectory
+
         self.loadPluginsByClass()
 
     def loadPluginsByClass(self):
-        #print '!! loadPluginsByClass !!'
-        #print 'toolsBaseDirectory', self.toolsBaseDirectory, 'toolsDirectory', self.toolsDirectory
-        #print 'toolsBaseDirectory', self.proToolsBaseDirectory, 'proToolsDirectory', self.proToolsDirectory
+
         self.allClasses = [cls for cls in
                            self.getAllModulesInFolder(self.toolsBaseDirectory, self.toolsDirectory)
                            if cls]
         self.allProClasses = [cls for cls in
-                           self.getAllModulesInFolder(self.proToolsBaseDirectory, self.proToolsDirectory)
+                           self.getAllModulesInFolder(self.proToolsBaseDirectory, self.proToolsDirectory, compiledOnly=True)
                            if cls]
-        #print 'allProClasses', self.allProClasses
+
         self.allClasses.extend(self.allProClasses)
         hotkeyClasses = [cls for cls in self.allClasses if cls.__base__ == hotKeyAbstractFactory]
         toolClasses = [cls for cls in self.allClasses if cls.__base__ == toolAbstractFactory]
 
         self.tools = dict()
         for cls in toolClasses:
-            #print ('instancing tool', cls, cls.toolName)
             self.tools[cls.toolName] = cls()
         self.loadedClasses['hotkeys'] = hotkeyClasses
 
-    def getAllModulesInFolder(self, baseDirectory, tooldDirectory):
+    def getAllModulesInFolder(self, baseDirectory, toolDirectory, compiledOnly=False):
         allFiles = list()
-        for (dirpath, dirnames, filenames) in os.walk(tooldDirectory):
+        for (dirpath, dirnames, filenames) in os.walk(toolDirectory):
             allFiles += [os.path.join(dirpath, file) for file in filenames]
-
+        ignored = ['__init__', '__pycache__']
         for file in allFiles:
-            if file not in ['__init__.py', '__pycache__']:  # skip unneeded files
-                if file[-3:] != '.py':
+            if any(x in file for x in ignored):  # skip unneeded files
+                continue
+
+            if compiledOnly:
+                if file.rsplit('.', 1)[-1] != 'pyc':
+                    continue
+            else:
+                if file.rsplit('.', 1)[-1] != 'py':
                     continue
 
-                file_name = os.path.basename(file.split('.')[0])
-                subFolder = str(os.path.relpath(os.path.dirname(file), tooldDirectory)).replace('\\', '.')
+            file_name = os.path.basename(file.split('.')[0])
+            subFolder = str(os.path.relpath(os.path.dirname(file), toolDirectory)).replace('\\', '.')
 
-                module_name = ('%s%s%s') % (baseDirectory, subFolder, file_name)
-                # hacky check to see if the script is in the start folder
-                if subFolder == '.':
-                    module_name = baseDirectory + '.' + file_name
-                else:
-                    module_name = baseDirectory + '.' + subFolder + '.' + file_name
-                for name, cls in inspect.getmembers(importlib.import_module(module_name), inspect.isclass):
-                    if cls.__module__ == module_name:
-                        yield cls
+            module_name = ('%s%s%s') % (baseDirectory, subFolder, file_name)
+            # hacky check to see if the script is in the start folder
+            if subFolder == '.':
+                module_name = baseDirectory + '.' + file_name
+            else:
+                module_name = baseDirectory + '.' + subFolder + '.' + file_name
+            for name, cls in inspect.getmembers(importlib.import_module(module_name), inspect.isclass):
+                if cls.__module__ == module_name:
+                    yield cls
 
     def collectQtMarkingMenuData(self, selection):
-        print ('collectQtMarkingMenuData', selection)
-
         menuDataDict = dict()
         if not selection:
             return None
