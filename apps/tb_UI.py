7d40c0c9dae50f43e88c31e62de0a26cc9187852
brimblashman@gmail.com <J33nLi4Lun>
2022-02-09 12:11:22 -0800

    Some more UI classes, help button, object line edits can verify themselves etc

--- apps/tb_UI.py
+++ apps/tb_UI.py
@@ -28,6 +28,7 @@ import maya.cmds as cmds
 import maya.OpenMayaUI as omUI
 import pymel.core as pm
 from functools import partial
+import subprocess
 
 qtVersion = pm.about(qtVersion=True)
 if int(qtVersion.split('.')[0]) < 5:
@@ -682,7 +683,6 @@ class TextInputWidget(QWidget):
         self.titleText.setAlignment(Qt.AlignCenter)
         self.text = QLabel(label)
 
-
         self.checkBoxWD = QCheckBox()
         self.checkBoxWD.setText(self.checkBox)
 
@@ -803,6 +803,7 @@ class TextInputWidget(QWidget):
         self.move(position_x, position_y)
         super(TextInputWidget, self).show()
 
+
 class InfoPromptWidget(QWidget):
     """
     Simple prompt with text input
@@ -818,6 +819,8 @@ class InfoPromptWidget(QWidget):
     def __init__(self, title=str(), label=str(), buttonText=str(), default=str(), combo=list(),
                  checkBox=None, overlay=False, showCloseButton=True, key=str(), subKey=str(),
                  helpString=None,
+                 image=str(),
+                 gif=bool,
                  parent=wrapInstance(int(omUI.MQtUtil.mainWindow()), QWidget)):
         super(InfoPromptWidget, self).__init__(parent=parent)
         self.showCloseButton = showCloseButton
@@ -846,6 +849,18 @@ class InfoPromptWidget(QWidget):
 
         sel = pm.ls(sl=True)
 
+        if image:
+            self.imageLabel = QLabel(self)
+            if gif:
+                self.movie = QMovie(os.path.join(IconPath, 'Help', image))
+
+                self.imageLabel.setMovie(self.movie)
+                self.movie.start()
+
+            else:
+                self.imagePixmap = QPixmap(os.path.join(IconPath, 'Help', image))
+                self.imageLabel.setPixmap(self.imagePixmap)
+
         self.titleText = QLabel(title)
         self.titleText.setAlignment(Qt.AlignCenter)
         self.text = QLabel(label)
@@ -871,22 +886,22 @@ class InfoPromptWidget(QWidget):
         self.helpLabel = QLabel(self.helpString)
         self.helpLabel.setWordWrap(True)
 
-
         titleLayout.addWidget(self.titleText)
         titleLayout.addWidget(self.closeButton, alignment=Qt.AlignRight)
         mainLayout.addLayout(titleLayout)
         mainLayout.addLayout(layout)
-        #layout.addWidget(self.text)
-        #layout.addWidget(self.lineEdit)
+        # layout.addWidget(self.text)
+        # layout.addWidget(self.lineEdit)
         if len(self.combo):
             layout.addWidget(self.comboBox)
         if self.checkBox is not None:
             layout.addWidget(self.checkBoxWD)
+
         if self.helpString:
             mainLayout.addWidget(self.helpLabel)
+        if image:
+            mainLayout.addWidget(self.imageLabel)
         mainLayout.addWidget(self.saveButton)
-
-
 
         self.setLayout(mainLayout)
         self.move(QApplication.desktop().availableGeometry().center() - self.rect().center())
@@ -965,6 +980,7 @@ class InfoPromptWidget(QWidget):
 
         self.move(position_x, position_y)
         super(InfoPromptWidget, self).show()
+
 
 class ChannelInputWidget(QWidget):
     """
@@ -1428,7 +1444,8 @@ class ChannelSelectLineEdit(QWidget):
     editedSignal = Signal(str)
     editedSignalKey = Signal(str, str)
 
-    def __init__(self, key=str(), text=str, tooltip=str(), lineEditWidth=200, placeholderTest=str(), stripNamespace=False):
+    def __init__(self, key=str(), text=str, tooltip=str(), lineEditWidth=200, placeholderTest=str(),
+                 stripNamespace=False):
         super(ChannelSelectLineEdit, self).__init__()
         self.stripNamespace = stripNamespace
         self.key = key
@@ -1480,6 +1497,38 @@ class ChannelSelectLineEdit(QWidget):
     def sendtextChangedSignal(self):
         self.editedSignal.emit(self.lineEdit.text())
         self.editedSignalKey.emit(self.key, self.lineEdit.text())
+
+
+class ChannelSelectLineEditEnforced(ChannelSelectLineEdit):
+    def __init__(self, key=str(), text=str(), tooltip=str(), lineEditWidth=200, placeholderTest=str(),
+                 stripNamespace=False, baseNamespace=str()):
+        super(ChannelSelectLineEditEnforced, self).__init__(key=key,
+                                                            text=text,
+                                                            tooltip=tooltip,
+                                                            lineEditWidth=lineEditWidth,
+                                                            placeholderTest=placeholderTest,
+                                                            stripNamespace=stripNamespace)
+
+        self.baseNamespace = baseNamespace
+        self.lineEdit.textChanged.connect(self.validateText)
+
+    def errorHighlight(self):
+        borderHighlightQSS = "QLineEdit {border: 2px solid QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #ffa02f, stop: 1 #d7801a)}"
+
+        self.lineEdit.setStyleSheet(borderHighlightQSS)
+
+    def errorHighlightRemove(self):
+        self.lineEdit.setStyleSheet(getqss.getStyleSheet())
+
+    def validateText(self):
+        node, attr = str(self.baseNamespace + ':' + self.lineEdit.text()).split('.')
+        if not cmds.objExists(node):
+            self.errorHighlight()
+            return
+        if not cmds.attributeQuery(attr, node=node, exists=True):
+            self.errorHighlight()
+        else:
+            self.errorHighlightRemove()
 
 
 class hotKeyWidget(QWidget):
@@ -1544,7 +1593,8 @@ class ObjectSelectLineEdit(QWidget):
     editedSignalKey = Signal(str, str)
 
     def __init__(self, key=str(), label=str(), hint=str(), labelWidth=65, lineEditWidth=200, placeholderTest=str(),
-                 stripNamespace=False):
+                 stripNamespace=False,
+                 tooltip=str()):
         QWidget.__init__(self)
         self.key = key
         self.stripNamespace = stripNamespace
@@ -1566,7 +1616,7 @@ class ObjectSelectLineEdit(QWidget):
         self.mainLayout.addWidget(self.itemLabel)
 
     def pickObject(self):
-        #print ('pickObject')
+        # print ('pickObject')
         sel = pm.ls(sl=True)
         if not sel:
             return
@@ -1586,6 +1636,33 @@ class ObjectSelectLineEdit(QWidget):
     @Slot()
     def textEdited(self):
         self.editedSignalKey.emit(self.key, self.itemLabel.text())
+
+
+class ObjectSelectLineEditEnforced(ObjectSelectLineEdit):
+    def __init__(self, key=str(), label=str(), hint=str(), labelWidth=65, lineEditWidth=200, placeholderTest=str(),
+                 stripNamespace=False,
+                 baseNamespace=str(),
+                 tooltip=str()):
+        super(ObjectSelectLineEditEnforced, self).__init__(key=key, label=label, hint=hint, labelWidth=labelWidth,
+                                                           lineEditWidth=lineEditWidth, placeholderTest=placeholderTest,
+                                                           stripNamespace=stripNamespace,
+                                                           tooltip=tooltip)
+        self.baseNamespace = baseNamespace
+        self.itemLabel.textChanged.connect(self.validateText)
+
+    def errorHighlight(self):
+        borderHighlightQSS = "QLineEdit {border: 2px solid QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #ffa02f, stop: 1 #d7801a)}"
+
+        self.itemLabel.setStyleSheet(borderHighlightQSS)
+
+    def errorHighlightRemove(self):
+        self.itemLabel.setStyleSheet(getqss.getStyleSheet())
+
+    def validateText(self):
+        if not cmds.objExists(self.baseNamespace + ':' + self.itemLabel.text()):
+            self.errorHighlight()
+        else:
+            self.errorHighlightRemove()
 
 
 class comboBoxWidget(QWidget):
@@ -1646,7 +1723,8 @@ class intFieldWidget(QWidget):
     changedSignal = Signal(float)
     editedSignalKey = Signal(str, float)
 
-    def __init__(self, key=str(), optionVar=None, defaultValue=int(), label=str(), minimum=0, maximum=1, step=0.1):
+    def __init__(self, key=str(), optionVar=None, defaultValue=int(), label=str(), minimum=0, maximum=1, step=0.1,
+                 tooltip=str()):
         QWidget.__init__(self)
         self.key = key
 
@@ -1756,10 +1834,12 @@ class radioGroupVertical(object):
 
 class LicenseWin(BaseDialog):
     ActivateSignal = Signal(str, str)
+    OfflineActivateSignal = Signal(str, str)
     leftClick = False
     oldPos = None
 
-    def __init__(self, parent=None, title='Tool Name?'):
+    def __init__(self, parent=None, title='Tool Name?', machineOnly=False,
+                 infoText='Please enter your license key and email address used for this purchase'):
         super(LicenseWin, self).__init__(parent=parent)
         self.setWindowTitle("LicenseWin!")
 
@@ -1768,6 +1848,9 @@ class LicenseWin(BaseDialog):
         # QBtn.button(QDialogButtonBox.Cancel).setText("Cancel")
         # self.buttonBox = QDialogButtonBox(QBtn)
         self.buttonBox = QDialogButtonBox()
+        if machineOnly:
+            offlineButton = self.buttonBox.addButton("Offline Activate", QDialogButtonBox.ActionRole)
+            offlineButton.clicked.connect(lambda: self.offlineActivate())
         self.buttonBox.addButton("Activate", QDialogButtonBox.AcceptRole)
         self.buttonBox.addButton("Cancel", QDialogButtonBox.RejectRole)
         self.buttonBox.accepted.connect(self.activate)
@@ -1782,9 +1865,9 @@ class LicenseWin(BaseDialog):
         self.setFixedSize(400, 180)
         self.gridLayout = QGridLayout()
         self.titleText.setText(title)
-        self.titleText.setStyleSheet("font-weight: bold; font-size: 14px;");
+        self.titleText.setStyleSheet("font-weight: bold; font-size: 14px;")
         self.titleText.setAlignment(Qt.AlignCenter | Qt.AlignTop)
-        self.infoText.setText('Please enter your license key and email address used for this purchase')
+        self.infoText.setText(infoText)
         self.infoText.setWordWrap(True)
 
         self.titleText.setAlignment(Qt.AlignCenter)
@@ -1828,10 +1911,40 @@ class LicenseWin(BaseDialog):
         return super(LicenseWin, self).keyPressEvent(event)
 
     def activate(self):
+        self.errorHighlightRemove(self.licenseLineEdit)
+        self.errorHighlightRemove(self.emailLineEdit)
+        if not len(self.licenseLineEdit.text()) == 35:
+            return cmds.warning('Invalid length license key')
+        if not '@' in self.emailLineEdit.text():
+            return cmds.warning('Invalid email address')
+        if not '.' in self.emailLineEdit.text():
+            return cmds.warning('Invalid email address')
         self.ActivateSignal.emit(self.licenseLineEdit.text(), self.emailLineEdit.text())
+
+    def offlineActivate(self):
+        self.errorHighlightRemove(self.licenseLineEdit)
+        self.errorHighlightRemove(self.emailLineEdit)
+        if not len(self.licenseLineEdit.text()) == 35:
+            self.errorHighlight(self.licenseLineEdit)
+            return cmds.warning('Invalid length license key')
+        if not '@' in self.emailLineEdit.text():
+            self.errorHighlight(self.emailLineEdit)
+            return cmds.warning('Invalid email address')
+        if not '.' in self.emailLineEdit.text():
+            self.errorHighlight(self.emailLineEdit)
+            return cmds.warning('Invalid email address')
+        self.OfflineActivateSignal.emit(self.licenseLineEdit.text(), self.emailLineEdit.text())
 
     def cancel(self):
         self.close()
+
+    def errorHighlight(self, widget):
+        borderHighlightQSS = "QLineEdit {border: 2px solid QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #ffa02f, stop: 1 #d7801a)}"
+
+        widget.setStyleSheet(borderHighlightQSS)
+
+    def errorHighlightRemove(self, widget):
+        widget.setStyleSheet(getqss.getStyleSheet())
 
 
 class PickObjectDialog(BaseDialog):
@@ -2100,6 +2213,21 @@ class MiniButton(QPushButton):
         self.setStyleSheet("background-color: transparent;border: 0px")
         self.setStyleSheet(getqss.getStyleSheet())
 
+class HelpButton(QPushButton):
+    def __init__(self, toolTip='Help', width=32, height=32):
+        super(HelpButton, self).__init__()
+        # self.setIcon(QIcon(":/{0}".format('closeTabButton.png')))
+        self.setFixedSize(width, height)
+
+        pixmap = QPixmap(":/help.png")
+        icon = QIcon(pixmap)
+
+        self.setIcon(icon)
+
+        self.setFlat(True)
+        self.setToolTip(toolTip)
+        self.setStyleSheet("background-color: transparent;border: 0px")
+        self.setStyleSheet(getqss.getStyleSheet())
 
 class LockButton(MiniButton):
     lockSignal = Signal(bool)
@@ -2504,3 +2632,170 @@ class Overlay(QWidget):
 
         self.widget.move(position_x, position_y)
         event.accept()
+
+
+class OfflineActivateInputWidget(QWidget):
+    """
+    Simple prompt with text input
+    """
+    acceptedSignal = Signal(str, str, str)
+
+    rejectedSignal = Signal()
+    oldPos = None
+
+    def __init__(self, title='Input activation code', label=str(), buttonText=str(), default=str(),
+                 overlay=False, showCloseButton=True, licenseVal=str(), emailVal=str(),
+                 productStr=str(),
+                 helpString=None,
+                 parentWidget=None,
+                 lineEditPlaceholder=str(),
+                 parent=wrapInstance(int(omUI.MQtUtil.mainWindow()), QWidget)):
+        super(OfflineActivateInputWidget, self).__init__(parent=parent)
+        self.showCloseButton = showCloseButton
+        self.parentWidget = parentWidget
+        self.key = licenseVal
+        self.subKey = emailVal
+        self.helpString = helpString
+        self.overlay = overlay
+
+        self.setWindowOpacity(1.0)
+        self.setWindowFlags(Qt.PopupFocusReason | Qt.Tool | Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_StyledBackground, True)
+        self.autoFillBackground = True
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.windowFlags()
+        self.setWindowTitle('Custom')
+        self.setFocusPolicy(Qt.StrongFocus)
+        # self.setFixedSize(400, 64)
+        titleLayout = QHBoxLayout()
+        mainLayout = QVBoxLayout()
+        layout = QHBoxLayout()
+
+        self.closeButton = MiniButton()
+        self.closeButton.clicked.connect(self.close)
+
+        sel = pm.ls(sl=True)
+
+        self.titleText = QLabel(title)
+        self.titleText.setAlignment(Qt.AlignCenter)
+        self.text = QLabel(label)
+
+        productLineEditLayout = QHBoxLayout()
+
+        self.productLineEdit = QLineEdit(productStr)
+        self.productLineEdit.setReadOnly(True)
+        self.productLineEdit.setFixedWidth(400)
+        self.copyToCLipButton = QPushButton('Copy to clipboard')
+        productLineEditLayout.addWidget(self.productLineEdit)
+        productLineEditLayout.addWidget(self.copyToCLipButton)
+        self.copyToCLipButton.clicked.connect(self.copyText)
+
+        self.infoText = QLabel(
+            'Copy the above text and email to to <b>tbanimtools@gmail.com</b> to get an offline activation code. <br>'
+            'Make sure your email and license key are correct. <br><br>'
+            'Once you get a response code, paste it into the box below')
+        self.infoText.setWordWrap(True)
+        self.saveButton = QPushButton(buttonText)
+        self.saveButton.setStyleSheet(getqss.getStyleSheet())
+
+        titleLayout.addWidget(self.titleText)
+        titleLayout.addWidget(self.closeButton, alignment=Qt.AlignRight)
+        mainLayout.addLayout(titleLayout)
+        mainLayout.addLayout(productLineEditLayout)
+        mainLayout.addWidget(self.infoText)
+        mainLayout.addLayout(layout)
+        layout.addWidget(self.text)
+
+        self.lineEdit = QLineEdit(default)
+        self.lineEdit.setFocusPolicy(Qt.StrongFocus)
+        reg_ex = QRegExp("[a-z-A-Z0123456789_:]+")
+        input_validator = QRegExpValidator(reg_ex, self.lineEdit)
+        self.lineEdit.setValidator(input_validator)
+
+        layout.addWidget(self.lineEdit)
+        layout.addWidget(self.saveButton)
+
+        if self.helpString:
+            self.helpLabel = QLabel(self.helpString)
+            self.helpLabel.setWordWrap(True)
+            mainLayout.addWidget(self.helpLabel)
+        self.saveButton.clicked.connect(self.acceptedFunction)
+
+        self.setLayout(mainLayout)
+        self.move(QApplication.desktop().availableGeometry().center() - self.rect().center())
+        self.show()
+        self.lineEdit.setFocus()
+        self.setStyleSheet(
+            "TextInputWidget { "
+            "border-radius: 8;"
+            "}"
+        )
+
+        self.closeButton.setVisible(self.showCloseButton)
+        self.resize(self.productLineEdit.sizeHint().width() + 48, self.sizeHint().height())
+        self.resize(self.sizeHint())
+        self.setStyleSheet(getqss.getStyleSheet())
+        # self.setFixedSize(400, 64)
+
+    def copyText(self):
+        cmd = 'echo ' + self.productLineEdit.text().strip() + '|clip'
+        return subprocess.check_call(cmd, shell=True)
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+
+        lineColor = QColor(68, 68, 68, 128)
+
+        # qp.setCompositionMode(qp.CompositionMode_Clear)
+        qp.setCompositionMode(qp.CompositionMode_Source)
+        qp.setRenderHint(QPainter.Antialiasing)
+
+        qp.setPen(QPen(QBrush(lineColor), 2))
+        grad = QLinearGradient(200, 0, 200, 32)
+        grad.setColorAt(0, "#323232")
+        grad.setColorAt(0.1, "#373737")
+        grad.setColorAt(1, "#323232")
+        qp.setBrush(QBrush(grad))
+        qp.drawRoundedRect(self.rect(), 16, 16)
+        qp.end()
+
+    def acceptedFunction(self, *args):
+        if not len(self.lineEdit.text()) == 8:
+            return cmds.warning('Wrong length for activation code')
+        self.acceptedSignal.emit(self.lineEdit.text(), self.key, self.subKey)
+
+    def close(self):
+        self.rejectedSignal.emit()
+        if self.overlay:
+            self.parent().close()
+        super(OfflineActivateInputWidget, self).close()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Return:
+            self.acceptedFunction()
+        if event.key() == Qt.Key_Escape:
+            self.close()
+        return super(OfflineActivateInputWidget, self).keyPressEvent(event)
+
+    def mousePressEvent(self, event):
+        self.oldPos = event.globalPos()
+
+    def mouseMoveEvent(self, event):
+        if not self.oldPos:
+            return
+        if self.overlay:
+            return
+        delta = QPoint(event.globalPos() - self.oldPos)
+        self.move(self.x() + delta.x(), self.y() + delta.y())
+        self.oldPos = event.globalPos()
+
+    def mouseReleaseEvent(self, event):
+        self.oldPos = None
+
+    def show(self):
+        position_x = (self.parent().pos().x() + (self.parent().width() - self.frameGeometry().width()) / 2)
+        position_y = (self.parent().pos().y() + (self.parent().height() - self.frameGeometry().height()) / 2)
+
+        self.move(position_x, position_y)
+        super(OfflineActivateInputWidget, self).show()
